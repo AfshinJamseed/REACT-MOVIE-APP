aa6fa05788df4049d65646a29e82610d5420552c
AfshinJamseed <afjkiller@gmail.com>
2026-06-05 17:10:19 +0530

    completed

---
+++ frontend/src/context/MovieContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useState, useEffect, useContext, Children } from "react";
+
+const MovieContext = createContext();
+
+export const useMovieContext = () => useContext(MovieContext);
+
+export const MovieProvider = ({children}) => {
+    const [favorites, setFavorites] = useState([]);
+
+    useEffect(() => {
+        const storedFavs = localStorage.getItem('favorites');
+
+        if (storedFavs) {
+            setFavorites(JSON.parse(storedFavs))
+        }
+    },[])
+    
+    useEffect(() => {
+        localStorage.setItem('favorites',JSON.stringify(favorites));
+    }, [favorites])
+
+    const addToFavorites = (movie) => {
+        setFavorites(previous => [...previous, movie])
+    }
+
+    const removeFromFavorites = (movieId) => {
+        setFavorites(previous => previous.filter(movie => movie.id !== movieId))
+    }
+
+    const isFavorite = (movieId) => {
+        return favorites.some(movie => movieId === movie.id)
+    }
+
+    const value = {
+        favorites,
+        addToFavorites,
+        removeFromFavorites,
+        isFavorite
+    }
+
+    return(
+        <MovieContext.Provider value={value}>
+            {children}
+        </MovieContext.Provider>
+    )
+}
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,31 @@
+import "../css/Favorites.css";
+import favoriteMovies from "../components/MovieCard";
+import { useState } from "react";
+import MovieCard from "../components/MovieCard";
+import { useMovieContext } from "../context/MovieContext";
+
+function Favorites() {
+  const {favorites} = useMovieContext();
+  if (favorites.length > 0) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => {
+            return <MovieCard movie={movie} key={movie.id} />;
+          })}
+        </div>
+      </div>
+    );
+  } else {
+  return (
+    <>
+      <div className="favorites-empty">
+        <h2>No Favorite Movies Yet</h2>
+        <p>Start adding movies to favorites</p>
+      </div>
+    </>
+  );
+}}
+
+export default Favorites;
